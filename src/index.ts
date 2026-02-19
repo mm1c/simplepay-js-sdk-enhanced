1cd197837de698982b98ef63355b081a10645860
ammalaci <ammalaci@gmail.com>
2026-02-19 08:26:29 +0100

    added missing cancelTransaction import

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import { startPayment } from './oneTime'
+import { cancelTransaction, startPayment } from './oneTime'
 import { startRecurringPayment, startTokenPayment, cancelCard} from './recurring'
 import type { Currency, Language, PaymentMethod, SimplePayResponse, SimplePayRecurringResponse, SimplePayTokenResponse, SimplePayCancelCardResponse, SimplePayResult, SimplePayIPNResponse } from './types'
 import { checkSignature, generateSignature, getPaymentResponse, handleIpnRequest, toISO8601DateString } from './utils'
 
 export {
-    Currency, Language, PaymentMethod,
-    SimplePayResponse, SimplePayRecurringResponse, SimplePayTokenResponse, SimplePayCancelCardResponse, SimplePayResult, SimplePayIPNResponse,
-    checkSignature, generateSignature, toISO8601DateString, getPaymentResponse, handleIpnRequest,
-    startPayment,     
-    startRecurringPayment, startTokenPayment, cancelCard
-}
+  Currency,
+  Language,
+  PaymentMethod,
+  SimplePayResponse,
+  SimplePayRecurringResponse,
+  SimplePayTokenResponse,
+  SimplePayCancelCardResponse,
+  SimplePayResult,
+  SimplePayIPNResponse,
+  checkSignature,
+  generateSignature,
+  toISO8601DateString,
+  getPaymentResponse,
+  handleIpnRequest,
+  startPayment,
+  startRecurringPayment,
+  startTokenPayment,
+  cancelCard,
+  cancelTransaction,
+};
